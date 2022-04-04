127af8410986442d1c334c175e354b8acb953d39
Shelby Pankratz <pa4039pb@mnstate.edu>
2022-04-04 09:41:50 -0500

    4/4

--- shelby.R
+++ shelby.R
@@ -1,18 +1,11 @@
 library(tidyverse)
 
-age_groups_of_people_fully_vaccinated <- 
-  read_csv("data/age_groups_of_people_fully_vaccinated.csv", skip=3, 
-           col_names = c("age_group", "fully_vaccinated"), 
-           col_types="c_i___") %>% 
-  print()
-
-order <- c("5-11", "12-17", "18-24", "25-39", 
-           "40-49", "50-64", "65-74", "75+")
+if (!dir.exists("figs")) dir.create("figs")
 
 ## vaccination
 # raw data
 vax_data <- 
-  read_csv("data/COVID-19_Vaccination_Demographics_in_the_United_States_National.csv") %>% 
+  read_csv("data/COVID-19_Vaccination_Demographics_in_the_United_States_National (1).csv") %>% 
   filter(
     str_starts(Demographic_category, "Ages_")
   ) %>% 
@@ -34,17 +27,19 @@ vax_data <-
 vax_data %>% 
   filter(date == max(date)) %>% 
   ggplot()+
-  geom_col(mapping = aes(x=age_group, per_vax), 
+  geom_col(mapping = aes(x=age_group, y=per_vax), 
            fill = "#a6192e") +
-  labs(y="% Fully Vaccinated", x="Age Range", title = "Vaccination Status as of March 27, 2022")
-
-  # + geom_jitter(size=3)+
-  theme_gray(base_size = 24)
+  labs(y="% Fully Vaccinated", x="Age Range", title = "Vaccination Status as of March 27, 2022")+
+  theme_gray(base_size = 16)
+ggsave("figs/vax data.png", height = 6, width = 10, units="in", dpi=600)
 
 # line graph 
 vax_data %>% 
   ggplot(aes(x = date, y = per_vax, color = age_group)) +
-  geom_line()
+  geom_line() +
+  theme_gray(base_size = 16)
+ggsave("figs/line vax data.png", height = 6, width = 10, units="in", dpi=600)
+
     
 ## cases
 # raw data w age groups
@@ -68,8 +63,11 @@ cases_by_age %>%
 
 # multiple graphs 
 cases_by_age %>% 
-  filter(date == as.Date("2022-03-26") | date == as.Date("2021-12-01")) %>% 
+  filter(date == as.Date("2022-03-26") | date == as.Date("2021-03-01")) %>% 
 ggplot() +
   geom_col(mapping = aes(x = date, y = total_cases),
            fill = "#a6192e") +
-  facet_wrap(~ age_range)
+  facet_wrap(~ age_range)+
+  theme_gray(base_size = 16)+
+  theme(axis.text.x=element_text(size=rel(.8)))
+ggsave("figs/case data.png", height = 8, width = 12, units="in", dpi=600)
